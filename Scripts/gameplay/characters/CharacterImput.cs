2add71702ccf853a6ea5ae70186697db4a59b669
Benjamin Wahl <benjamin.wahl@associatedterminals.com>
2026-08-25 01:37:30 -0500

    Missing files as of 8_25_2026

---
+++ Scripts/gameplay/characters/CharacterImput.cs
@@ -0,0 +1,25 @@
+using Godot;
+using Game.Core;
+using System;
+using System.Runtime.InteropServices.JavaScript;
+
+
+namespace Game.Gameplay
+{
+
+
+
+    public abstract partial class CharacterInput : Node
+    {   
+        [Signal] public delegate void WalkEventHandler();
+        [Signal] public delegate void TurnEventHandler();
+
+
+       [ExportCategory("Common Input")]
+       [Export] public Vector2 Direction = Vector2.Zero;
+       [Export] public Vector2 TargetPosition = Vector2.Zero;
+
+
+    }
+
+}
